022f0e05a03862c7139d7c98253b140cfd252911
Srikar Yekollu <srikary@gmail.com>
2015-08-08 20:28:07 -0700

    Adding Servo and Stepper driver modules

---
+++ modules/drivers/servo.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+import time
+
+
+
+class Servo:
+  """ Wrapper interface to a Servo."""
+  
+  def __init__(self, bcm_pin, init_pos=0, move_delay_ms=0.01):
+    """
+        bcm_pin: Provide the pin (BCM numbering) that the Servo is connected to.
+        init_pos: A number between 0 and 180 that specifies the initial angle
+        move_delay: parameter in the constructor
+    """
+    self.move_delay = move_delay
+    if (init_pos < 0 or init_pos > 180):
+      raise ValueError("Initial position invalid:" + str(init_pos))
+
+    self.curr_pos = init_pos
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(bcm_pin, GPIO.OUT)
+    self.pwm = GPIO.PWM(self.bcm_pin, 100)
+    self.pwm.start(get_dutycycle_for_angle(init_pos))
+
+  def get_dutycycle_for_angle(angle):
+    return float(angle) / 36 + 5 # (float(angle)/180 + 1) * 5
+  
+  def set_angle(angle):
+    duty = get_dutycycle_for_angle(angle)
+    pwm.ChangeDutyCycle(duty)
+
+  def move_to(self, dest_angle):
+    if (dest_angle< 0 or dest_angle > 180):
+      raise ValueError("Destination position invalid:" + str(dest_angle))
+    direction = 1
+    if (dest_angle < self.curr_pos):
+      direction = -1
+    for angle in range(self.curr_pos, dest_pos, direction):
+      self.curr_pos = angle
+      set_angle(self.curr_pos)
+      time.sleep(self.move_delay)
+          
+if (__name__ == "__main__"):
+  servo = Servo(3, 0)
+  while True:
+    servo.move_to(180)
+    servo.move_to(0)
+    
---
+++ modules/drivers/stepper.py
@@ -0,0 +1,68 @@
+import RPi.GPIO as GPIO
+import time
+
+class StepperMotor:
+  """ Wrapper interface to a stepper motor. """
+
+  steps_per_rotation = 200
+  min_delay_per_step = 0
+  
+  def __init__(self, dir_pin, step_pin, speed=60):
+    self.delay_bw_steps = calc_delay_for_speed(speed)
+    GPIO.setmode(GPIO.BCM)
+    self.direction_pin = dir_pin
+    self.step_pin = step_pin
+    GPIO.setup(self.direction_pin, GPIO.OUT)
+    GPIO.setup(self.step_pin, GPIO.OUT)
+    GPIO.output(self.step_pin, GPIO.LOW)
+    self.delay_per_step = get_step_delay_from_speed(speed)
+    
+  def get_step_delay_from_speed(speed):
+    """ Converts the speed (in rpm) to the delay between each step """
+    
+    rotations_per_second = speed / 60
+    steps_per_second = rotations_per_second * steps_per_rotation
+    time_per_step = float(1.0)/steps_per_second
+    return time_per_step - min_delay_per_step
+  
+  def rotate(self, dir_is_clockwise, num_rotations):
+    if dir_is_clockwise:
+      GPIO.output(self.direction_pin, GPIO.HIGH)
+    else:
+      GPIO.output(self.direction_pin, GPIO.LOW)
+
+    num_steps = num_rotations * steps_per_rotation
+    for i in range(0, num_steps):
+      GPIO.output(self.step_pin, GPIO.HIGH)
+      time.sleep(self.delay_per_step)
+      GPIO.output(self.step_pin, GPIO.LOW)
+
+  def set_speed(self, speed):
+    self.delay_per_step = get_step_delay_from_speed(speed)
+    
+  def rotate_at_speed(self, speed, dir_is_clockwise, num_rotations):
+    self.set_speed(speed)
+    self.rotate(dir_is_clockwise, num_rotations)
+    
+  def rotate_clockwise(self, speed, num_rotations):
+    self.rotate_at_speed(speed, True, num_rotations)
+
+  def rotate_anticlockwise(self, speed, num_rotations):
+    self.rotate_at_speed(speed, False, num_rotations)
+
+  def rotate_clockwise(self, num_rotations):
+    self.rotate(True, num_rotations)
+
+  def rotate_anticlockwise(self, num_rotations):
+    self.rotate(False, num_rotations)
+      
+if (__name__ == "__main__"):
+  stepper = StepperMotor(3, 4, 60)
+  while True:
+    stepper.rotate_clockwise(3)
+    stepper.rotate_anticlockwise(3)
+    stepper.rotate_clockwise(3)
+    stepper.set_speed(120)
+    stepper.rotate_clockwise(3)
+    stepper.rotate_anticlockwise(3)
+    stepper.rotate_clockwise(3)
