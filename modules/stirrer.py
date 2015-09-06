a06a98a68612f2ade0f1c33ae89e057a4f28ba4a
Srikar Yekollu <srikary@gmail.com>
2015-09-06 23:51:26 +0000

    checkpoint

--- modules/stirrer.py
+++ modules/stirrer.py
@@ -1,120 +1,124 @@
-import modules.submodules.stepper_axis as stepper_axis
-from enum import Enum
+import submodules.stepper_axis as stepper_axis
 import math
 import time
 
+class PlatformPosition:
+  BASE = 1
+  UTENSIL = 2
+  LID = 3
+
+def get_curr_time_in_secs():
+  return time.mktime(time.localtime())
+
 class Stirrer:
   # Dimensions of the Rails
-  max_x_rail_translation_mm = 100.0
-  max_y_rail_translation_mm = 100.0
-  max_z_rail_translation_mm = 100.0
+  max_x_rail_translation_mm = 380.0
+  max_y_rail_translation_mm = 325.0
+  max_z_rail_translation_mm = 300.0
 
-  stirrer_z_up_pos = 0.0
-  stirrer_z_down_pos = 120.0
-  stirrer_x_utensil_pos = 50.0
-  stirrer_y_utensil_pos = 50.0
+  z_up_pos = 0.0
+  z_down_pos = 120.0
+  x_utensil_pos = 190.0
+  y_utensil_pos = 162.0
 
-  stirrer_x_lid_utensil_pos = 100.0
-  stirrer_y_lid_utensil_pos = 100.0
+  x_lid_utensil_pos = 190.0
+  y_lid_utensil_pos = 192.0
 
-  stirrer_x_home_pos = 0.0
-  stirrer_y_home_pos = 0.0
+  x_home_pos = 5.0
+  y_home_pos = 5.0
 
   stirrer_width_mm = 50.0
 
-  stir_start_gap = 5.0
-  stir_stop_gap = 10.0
+  stir_start_gap = 5.0 # Distance from utensil wall where the stirrer starts a stroke.
+  stir_stop_gap = 20.0 # Distance from utensil wall where the stirrer stops during a stroke.
 
-  class PlatformPosition(Enum):
-    BASE = 1
-    UTENSIL = 2
-    LID = 3
-    
   # Diameters of the three different all-clad utensils
   utensil_diameter_mm = [270.0, 300.0, 150.0]
-  
+
   def __init__(self,
                x_rail_dir_pin, x_rail_step_pin,
                y_rail_dir_pin, y_rail_step_pin,
                z_rail_dir_pin, z_rail_step_pin):
     # TODO : Fill in values to the constructor below.
-    self.x_rail = stepper_axis.StepperAxis(x_rail_dir_pin, x_rail_step_pin, max_x_rail_translation_mm)
-    self.y_rail = stepper_axis.StepperAxis(y_rail_dir_pin, y_rail_step_pin, max_y_rail_translation_mm)
-    self.z_rail = stepper_axis.StepperAxis(z_rail_dir_pin, z_rail_step_pin, max_z_rail_translation_mm)
+    self.x_rail = stepper_axis.StepperAxis(x_rail_dir_pin, x_rail_step_pin, Stirrer.max_x_rail_translation_mm)
+    self.y_rail = stepper_axis.StepperAxis(y_rail_dir_pin, y_rail_step_pin, Stirrer.max_y_rail_translation_mm)
+    self.z_rail = stepper_axis.StepperAxis(z_rail_dir_pin, z_rail_step_pin, Stirrer.max_z_rail_translation_mm)
     self.platform_position = PlatformPosition.BASE
-    
-  def stirrer_up(self):
-    self.z_rail.move_to(stirrer_z_up_pos)
-
-  def stirrer_down(self):
-    self.z_rail.move_to(stirrer_z_down_pos)
-
-  def is_stirrer_up(self):
-    return self.z_rail.get_curr_pos_mm == stirrer_z_up_pos
- 
-  def position_platform_at_utensil(self):
-    self.stirrer_up()
-    self.x_rail.move_to(stirrer_x_utensil_pos)
-    self.y_rail.move_to(stirrer_y_utensil_pos)
-    self.platform_position = PlatformPosition.UTENSIL
-    
-  def position_platform_at_base(self):
-    self.stirrer_up()
-    self.x_rail.move_to(stirrer_x_home_pos)
-    self.y_rail.move_to(stirrer_y_home_pos)
-    self.platform_position = PlatformPosition.UTENSIL
-    
-  def position_platform_at_lid(self):
-    self.stirrer_up()
-    self.x_rail.move_to(stirrer_x_lid_utensil_pos)
-    self.y_rail.move_to(stirrer_y_lid_utensil_pos)
-    self.platform_position = PlatformPosition.LID
-    
-  def get_cord_length_mm(dist_from_center, utensil_index):
-    if utensil_index >= 3:
-      raise ValueError("There are only three known utensil sizes. Invalid utensil index:" + utensil_index)
-
-    utensil_radius = utensil_diameter_mm[utensil_index]/2
-    if (dist_from_center >= utensil_radius):
-      raise ValueError("Requested distance from center is invalid for specified utensil:" + dist_from_center)
-
-    return 2 * math.sqrt((utensil_radius * utensil_radius) - (dist_from_center * dist_from_center))
 
   def move_to(self, dest_pos):
     start_pos = (self.x_rail.get_curr_pos_mm(), self.y_rail.get_curr_pos_mm())
     delta = ((dest_pos[0] - self.x_rail.get_curr_pos_mm()),
              (dest_pos[1] - self.y_rail.get_curr_pos_mm()))
     max_delta = max(abs(delta[0]), abs(delta[1]))
-    
-    for i in range(1, max_delta + 1):
+
+    for i in range(1, int(max_delta + 1)):
       next_x = start_pos[0] + ((float(i) / max_delta) * delta[0])
       next_y = start_pos[1] + ((float(i) / max_delta) * delta[1])
       self.x_rail.move_to(next_x)
       self.y_rail.move_to(next_y)
-    
+
   def execute_stir_stroke(self, start_pos, end_pos):
     self.stirrer_up()
     self.move_to(start_pos)
     self.stirrer_down()
     self.move_to(end_pos)
     self.stirrer_up()
-    
+
+  def get_cord_length_mm(self, dist_from_center, utensil_index):
+    if utensil_index >= 3:
+      raise ValueError("There are only three known utensil sizes. Invalid utensil index:" + utensil_index)
+
+    utensil_radius = Stirrer.utensil_diameter_mm[utensil_index]/2
+    if (dist_from_center >= utensil_radius):
+      raise ValueError("Requested distance from center is invalid for specified utensil:" + dist_from_center)
+
+    return 2 * math.sqrt((utensil_radius * utensil_radius) - (dist_from_center * dist_from_center))
+
   def one_stir_stroke(self, dist_from_center, utensil_index, top_to_bottom):
-    cord_length = get_cord_length_mm(dist_from_center, utensil_index)
-    start_x = stirrer_x_home_pos - dist_from_center
+    cord_length = self.get_cord_length_mm(dist_from_center, utensil_index)
+    start_x = Stirrer.x_home_pos - dist_from_center
     if dist_from_center > 0:
-      start_x = start_x + (stirrer_width_mm / 2)
+      start_x = start_x + (Stirrer.stirrer_width_mm / 2)
     else:
-      start_x = start_x - (stirrer_width_mm / 2)
+      start_x = start_x - (Stirrer.stirrer_width_mm / 2)
 
     if top_to_bottom:
-      start_y = stirrer_y_home_pos + (cord_length / 2) - stir_start_gap
-      end_y = start_y - cord_length + stir_stop_gap
+      start_y = Stirrer.y_home_pos + (cord_length / 2) - Stirrer.stir_start_gap
+      end_y = start_y - cord_length + Stirrer.stir_stop_gap
     else:
-      start_y = stirrer_y_home_pos - (cord_length / 2) + stir_start_gap
-      end_y = start_y + cord_length - stir_stop_gap
+      start_y = Stirrer.y_home_pos - (cord_length / 2) + Stirrer.stir_start_gap
+      end_y = start_y + cord_length - Stirrer.stir_stop_gap
 
     self.execute_stir_stroke((start_x, start_y), (start_x, end_y))
+
+  ################  Public methods. #################
+
+  def stirrer_up(self):
+    self.z_rail.move_to(Stirrer.z_up_pos)
+
+  def stirrer_down(self):
+    self.z_rail.move_to(Stirrer.z_down_pos)
+
+  def is_stirrer_up(self):
+    return self.z_rail.get_curr_pos_mm == Stirrer.z_up_pos
+
+  def position_platform_at_utensil(self):
+    self.stirrer_up()
+    self.x_rail.move_to(Stirrer.x_utensil_pos)
+    self.y_rail.move_to(Stirrer.y_utensil_pos)
+    self.platform_position = PlatformPosition.UTENSIL
+
+  def position_platform_at_base(self):
+    self.stirrer_up()
+    self.x_rail.move_to(Stirrer.x_home_pos)
+    self.y_rail.move_to(Stirrer.y_home_pos)
+    self.platform_position = PlatformPosition.UTENSIL
+
+  def position_platform_at_lid(self):
+    self.stirrer_up()
+    self.x_rail.move_to(Stirrer.x_lid_utensil_pos)
+    self.y_rail.move_to(Stirrer.y_lid_utensil_pos)
+    self.platform_position = PlatformPosition.LID
 
   def is_platform_at_base(self):
     return self.platform_position == PlatformPosition.BASE
@@ -124,32 +128,44 @@ class Stirrer:
 
   def is_platform_at_lid(self):
     return self.platform_position == PlatformPosition.LID
-  
-  def get_curr_time_in_secs():
-    return time.mktime(time.localtime())
-  
+
   # X axis is the top rail and increases left to right (looking FROM ATX)
   # Y axis is the bottom rail and increases from ATX to front.
   def stir(self, utensil_index, stir_for_seconds):
-    utensil_radius = utensil_diameter_mm[utensil_index]/2
+    self.position_platform_at_utensil()
+    self.stirrer_down()
+    utensil_radius = Stirrer.utensil_diameter_mm[utensil_index]/2
     stir_dx = -10
     top_to_bottom = True
-    start = get_curr_time_in_secs()
+    start_time = get_curr_time_in_secs()
     while True:
       current = get_curr_time_in_secs()
       if (current  - start_time) > stir_for_seconds:
         break
-      for dist_from_center in range((utensil_radius - stir_start_gap), -utensil_radius, stir_dx):
+      for dist_from_center in range(int(utensil_radius - Stirrer.stir_start_gap), int(-utensil_radius), stir_dx):
         self.one_stir_stroke(dist_from_center, utensil_index, top_to_bottom)
         top_to_bottom = not top_to_bottom
         current = get_curr_time_in_secs()
         if (current  - start_time) > stir_for_seconds:
           break
+
   def shutdown(self):
     self.position_platform_at_base()
-    
+
 if (__name__ == "__main__"):
-  stirrer = Stirrer(2, 3, 4, 5, 6, 7)
+  stirrer = Stirrer(7, 8,  # X Dir, Step
+                   11, 25, # Y Dir, Step
+                   9, 10)  # Z Dir, Step
+  stirrer.stir(0, 20)
   stirrer.position_platform_at_utensil()
+  print "At Utensil"
+  time.sleep(10)
+  stirrer.position_platform_at_lid()
+  print "At Lid"
+  time.sleep(10)
+  stirrer.stir(0, 20)
+  print "Done stirring"
+  time.sleep(10)
   stirrer.stirrer_goto_base()
-  stirrer.stir(0, 100)
+  print "At Base"
+  time.sleep(10)
--- modules/submodules/stepper_axis.py
+++ modules/submodules/stepper_axis.py
@@ -34,7 +34,7 @@ class StepperAxis:
 
   def move_to(self, new_pos_mm):
     if new_pos_mm > self.max_translation_mm or new_pos_mm < 0:
-      raise ValueError("Invalid value for new_pos_mm. Has to be within " +
+        raise ValueError("Invalid value:" + str(new_pos_mm) + " for new_pos_mm. Has to be within " +
                        " range(0," +str(self.max_translation_mm) + ")")
     if new_pos_mm > self.curr_pos_mm:
       self.increment_pos_by_mm(new_pos_mm - self.curr_pos_mm)
@@ -46,10 +46,10 @@ class StepperAxis:
     return self.curr_pos_mm
 
 if (__name__ == "__main__"):
-  y_stepper = StepperAxis(6, 5, 60)
-  x_stepper = StepperAxis(9, 10, 60)
-  z_stepper = StepperAxis(7, 8, 60)
-  r_stepper = StepperAxis(11, 25, 60)
+  r_stepper = StepperAxis(6, 5, 6000, inc_clockwise=False)
+  z_stepper = StepperAxis(9, 10, 6000)
+  x_stepper = StepperAxis(7, 8, 6000, inc_clockwise=False)
+  y_stepper = StepperAxis(11, 25, 6000)
   while True:
     inp = raw_input("-->")
     vals = inp.split()
@@ -58,14 +58,14 @@ if (__name__ == "__main__"):
     motor_num = int(vals[0])
     position = int(vals[1])
     if motor_num == 1:
-      y_stepper.move_to(position)
-      print "Y Currently at: " + str(y_stepper.get_curr_pos_mm())
-    elif motor_num == 2:
-      x_stepper.move_to(position)
-      print "X Currently at: " + str(x_stepper.get_curr_pos_mm())
-    elif motor_num == 3:
-      z_stepper.move_to(position)
-      print "Z Currently at: " + str(z_stepper.get_curr_pos_mm())
-    else:
       r_stepper.move_to(position)
       print "R Currently at: " + str(r_stepper.get_curr_pos_mm())
+    elif motor_num == 2:
+      z_stepper.move_to(position)
+      print "Z Currently at: " + str(z_stepper.get_curr_pos_mm())
+    elif motor_num == 3:
+      x_stepper.move_to(position)
+      print "X Currently at: " + str(x_stepper.get_curr_pos_mm())
+    else:
+      y_stepper.move_to(position)
+      print "Y Currently at: " + str(y_stepper.get_curr_pos_mm())
