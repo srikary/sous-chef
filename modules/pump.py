91821877884b4f775aeb016d463b8f51e01e2eb9
Srikar Yekollu <srikary@gmail.com>
2016-01-04 05:54:07 +0000

    pump

--- modules/pump.py
+++ modules/pump.py
@@ -72,5 +72,5 @@ if (__name__ == "__main__"):
   while True:
     inp = raw_input("-->")
     qty = float(inp)
-    oil_pump.run_pump_for_msec(qty)
-    #pump.dispense_cup(1)
+    #oil_pump.run_pump_for_msec(qty)
+    water_pump.run_pump_for_msec(qty)
